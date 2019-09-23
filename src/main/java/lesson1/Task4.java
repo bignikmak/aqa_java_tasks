8c6625b357a79860b504397ff28219941ddcc8c1
mak <bignikmak@gmail.com>
2019-09-24 00:44:43 +0300

    Edit task 2,3,4 and made recursion through cycle

--- src/main/java/lesson1/Task4.java
+++ src/main/java/lesson1/Task4.java
@@ -1,20 +1,18 @@
 package lesson1;
 
-/* Мое имя
-Вывести на экран свое имя 5 строк по 10 раз (через пробел).
-System.out.println, System.out.print можно использовать только по одному разу
-для этого нужно воспользоваться циклом do () while
-*/
-
 public class Task4 {
 
     public static void main(String[] args) {
         String s = "Nick";
         int i = 0;
         do {
-            System.out.print(s + " " + s + " " + s + " " + s + " " + s + " " + s + " " + s + " " + s + " " + s + " ");
-            System.out.println(s);
-            i++;
+            int j = 0;
+                do {
+                    System.out.print(s + " ");
+                    j++;
+                } while (j < 9);
+                System.out.println(s);
+                i++;
         } while (i < 5);
     }
 }
--- src/main/java/lesson1/Task5.java
+++ src/main/java/lesson1/Task5.java
@@ -1,25 +1,16 @@
 package lesson1;
 
-import jdk.internal.cmm.SystemResourcePressureImpl;
-import org.jetbrains.annotations.NotNull;
-
-/**
- * Реализовать функцию, которая принимает строку и возвращает ее же в обратном виде
- * например "Hello world!!!" -> "!!!dlrow olleH"
- */
 public class Task5 {
     public static void main(String[] args) {
-
-        String str = "Hello world!!!";
-        String r = reverse(str);
-        System.out.println(r);
+        System.out.println(reverse());
     }
-
-    public static String reverse(String str) {
-        if (str.isEmpty()){
-            return str;
-        }
-
-        return reverse(str.substring(1)) + str.charAt(0);
-        }
+   public static String reverse () {
+       String str = "Hello world!!!";
+       String res = " ";
+       int L = str.length();
+       for (int i = 0; i < L; i++) {
+           res = str.charAt(i) + res;
+       }
+       return res;
+    }
 }
