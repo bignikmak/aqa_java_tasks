5a8256b8b7dae6fece0d6e7c0364c1b7b548f93d
mak <bignikmak@gmail.com>
2019-09-23 18:16:44 +0300

    task 5 done

--- src/main/java/lesson1/Task5.java
+++ src/main/java/lesson1/Task5.java
@@ -1,15 +1,25 @@
 package lesson1;
 
+import jdk.internal.cmm.SystemResourcePressureImpl;
+import org.jetbrains.annotations.NotNull;
+
 /**
  * Реализовать функцию, которая принимает строку и возвращает ее же в обратном виде
  * например "Hello world!!!" -> "!!!dlrow olleH"
  */
 public class Task5 {
     public static void main(String[] args) {
-        //System.out.println(reverse("Hello world!!!"));
+
+        String str = "Hello world!!!";
+        String r = reverse(str);
+        System.out.println(r);
     }
 
     public static String reverse(String str) {
-        return "";
-    }
+        if (str.isEmpty()){
+            return str;
+        }
+
+        return reverse(str.substring(1)) + str.charAt(0);
+        }
 }
