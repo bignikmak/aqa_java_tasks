91ef19c4afb7723d2848b36c4bbbfac2993e3532
mak <bignikmak@gmail.com>
2019-09-23 15:46:22 +0300

    task 3 done

--- src/main/java/lesson1/Task1.java
+++ src/main/java/lesson1/Task1.java
@@ -8,14 +8,14 @@ public class Task1 {
     public static void main(String[] args) {
     }
 
-    public static int min(int a, int b, int c) {
-        int min;
-        if ((a < b) && (a < c))
-            min = a;
-         else if ((b < a) && ( b < c))
-            min = b;
-         else
-             min =c;
-        return min;
-    }
+        public static int min(int a, int b, int c) {
+            int min;
+            if ((a < b) && (a < c))
+                min = a;
+            else if ((b < a) && (b < c))
+                min = b;
+            else
+                min = c;
+            return min;
+        }
 }
--- src/main/java/lesson1/Task3.java
+++ src/main/java/lesson1/Task3.java
@@ -9,21 +9,12 @@ System.out.println, System.out.print можно использовать тол�
 public class Task3 {
 
     public static void main(String[] args) {
-        String s = "Vladimir";
-        System.out.println(
-                s + " " + s + " " + s + " " + s + " " + s + " " + s + " " + s + " " + s + " " + s + " "
-                        + s);
-        System.out.println(
-                s + " " + s + " " + s + " " + s + " " + s + " " + s + " " + s + " " + s + " " + s + " "
-                        + s);
-        System.out.println(
-                s + " " + s + " " + s + " " + s + " " + s + " " + s + " " + s + " " + s + " " + s + " "
-                        + s);
-        System.out.println(
-                s + " " + s + " " + s + " " + s + " " + s + " " + s + " " + s + " " + s + " " + s + " "
-                        + s);
-        System.out.println(
-                s + " " + s + " " + s + " " + s + " " + s + " " + s + " " + s + " " + s + " " + s + " "
-                        + s);
+        String s = "Nick";
+        int i = 0;
+        while (i < 5) {
+            System.out.print(s + " " + s + " " + s + " " + s + " " + s + " " + s + " " + s + " " + s + " " + s + " ");
+            System.out.println(s);
+            i++;
+        }
     }
 }
