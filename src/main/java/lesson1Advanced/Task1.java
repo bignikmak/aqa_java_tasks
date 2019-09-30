3ab86c9168facfeeddb2dcc440c2c1b6d23f058d
mak <bignikmak@gmail.com>
2019-09-30 17:14:07 +0300

    lesson1 advanced task 1,2

--- src/main/java/lesson1Advanced/Task1.java
+++ src/main/java/lesson1Advanced/Task1.java
@@ -1,15 +1,20 @@
 package lesson1Advanced;
 
+import java.util.Arrays;
+
+import static java.util.Arrays.*;
+
 public class Task1 {
-    /* Минимум n чисел
-    Написать функцию, которая вычисляет минимум из массива чисел.
-    */
     public static void main(String[] args) {
-        //System.out.println(min([2,4,6]));
-        //System.out.println(min([2,4,6,1,90,2,0]));
+//        System.out.println(min());
+//        System.out.println(Arrays.toString(ar));
     }
 
-    public static int min(int[] a) {
-        return 0;
+    public static void min(int[] ar) {
+ //       int[] ar = new int[] {2, 4, 6, 1, 90, 2, 0};
+
+ //       return ar[0];
     }
+
+
 }
