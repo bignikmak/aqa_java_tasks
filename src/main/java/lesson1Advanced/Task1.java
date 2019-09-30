687b1a1078ad4de7bcc8367e64c72ccfdbc98bab
mak <bignikmak@gmail.com>
2019-09-30 18:54:45 +0300

    lesson1 advanced task 1 done 2 ways

--- src/main/java/lesson1Advanced/Task1.java
+++ src/main/java/lesson1Advanced/Task1.java
@@ -2,19 +2,26 @@ package lesson1Advanced;
 
 import java.util.Arrays;
 
-import static java.util.Arrays.*;
-
 public class Task1 {
     public static void main(String[] args) {
-//        System.out.println(min());
-//        System.out.println(Arrays.toString(ar));
+        int a = 2;
+        int b = 4;
+        int c = 6;
+        int d = 1;
+        int e = 90;
+        int f = 2;
+        int g = 0;
+        System.out.println(min(a, b, c, d, e, f, g));
+        System.out.println(min2(a, b, c));
     }
 
-    public static void min(int[] ar) {
- //       int[] ar = new int[] {2, 4, 6, 1, 90, 2, 0};
-
- //       return ar[0];
+    public static int min(int a, int b, int c, int d, int e, int f, int g) {
+        int[] ar = {a, b, c, d, e, f, g};
+        Arrays.sort(ar);
+        return ar[0];
     }
 
-
+    public static int min2(int a, int b, int c) {
+        return (a < b) ? ((a < c) ? a : c) : ((b < c) ? b : c);
+    }
 }
